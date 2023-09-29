32b7ee2a69c959a572178cce852e098039c8493d
Simon Lau <simon.lau.sg@gmail.com>
2023-09-29 12:09:48 +0800

    build: setup proxy server for dev

--- server.js
+++ server.js
@@ -1,16 +1,29 @@
+//* import -> require
 const express = require("express");
+const path = require("path");
 const logger = require("morgan");
 const debug = require("debug")("mern:server");
 
+//* app
 const app = express();
 
+//* middleware
 app.use(logger("dev"));
 app.use(express.json());
+app.use(express.static(path.join(__dirname, "dist")));
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+//* routes -> all express routes start with /api
+app.get("/api", (req, res) => {
+  res.json({ msg: "Hello World!" });
 });
 
+//? just for react router
+app.get("/*", (req, res) => {
+  res.sendFile(path.join(__dirname, "dist", "index.html"));
+});
+
+//* listen
+//? Heroku opens Port 3300
 const port = process.env.PORT || 3000;
 
 app.listen(port, function () {
