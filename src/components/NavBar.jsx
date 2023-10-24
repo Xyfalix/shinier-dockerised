d522c080a59a969e38e34e35e263dac978907712
Xyfalix <129175727+Xyfalix@users.noreply.github.com>
2023-10-24 10:18:03 +0800

    feat(nicholas): style navbar (in progress)

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,5 +1,3 @@
-// Don't forget the import
-import { Link } from "react-router-dom";
 import { logout } from "../utilities/users-service";
 
 export default function NavBar({user, setUser}) {
@@ -11,12 +9,20 @@ export default function NavBar({user, setUser}) {
   };
 
   return (
-    <nav>
-      <p>Hi, {user.name}, {user.email}</p>
-      <Link to="/orders">Order History</Link>
-      &nbsp; | &nbsp;
-      <Link to="/orders/new">New Order</Link>
-      <button onClick={handleLogout}>Logout</button>
+    <nav className="bg-primary text-primary-content">
+      <div className="flex justify-between">
+        <div className="flex flex-row items-center">
+          <a className="btn btn-ghost normal-case text-2xl">Shinier</a>
+          <p className="text-lg mx-4">Pokemon</p>
+        </div>
+        <div className="flex flex-row items-center">
+          <p className="mx-2">My Orders</p>
+          <p className="mx-2">🛍️</p>
+          <p className="mx-2 text-2xl">♡</p>
+          <p className="text-lg mx-2">👤 {user.name}</p>
+          <button className="mx-2" onClick={handleLogout}>Logout</button>
+        </div>
+      </div>
     </nav>
   );
 }
