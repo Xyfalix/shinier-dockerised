a1a93fe1354f02e862f6cce398e99121ddcb1dd3
Xyfalix <129175727+Xyfalix@users.noreply.github.com>
2023-10-25 11:25:05 +0800

    feat(nicholas): add randomized card images to landing page

--- src/pages/AppLandingPage/AppLanding.jsx
+++ src/pages/AppLandingPage/AppLanding.jsx
@@ -1,18 +1,34 @@
 import debug from "debug";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "../App/App.css"
 import NavBar from "../../components/NavBar";
 import { getUser } from "../../utilities/users-service";
+import cardImageData from "../../cardImageData.json"
 
 const log = debug("mern:src:App");
 localStorage.debug = "mern:*";
 
 log("Start React App");
 
-export default function AppMain() {
+export default function AppLanding() {
   const [user, setUser] = useState(getUser);
   const [searchInput, setSearchInput] = useState("");
-  const imageIds = [237, 238, 239, 240, 241, 242];
+  const [landingCardImages, setLandingCardImages] = useState([])
+
+  // Function to shuffle an array randomly (Fisher-Yates shuffle)
+  const shuffleArray = (array) => {
+    for (let i = array.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]];
+    }
+  };
+
+  useEffect(() => {
+    const data = cardImageData.data;
+    shuffleArray(data);
+    const randomImages = data.slice(0, 6);
+    setLandingCardImages(randomImages);
+  }, []);
 
   return (
     <main className="App">
@@ -31,16 +47,16 @@ export default function AppMain() {
               onChange={(e) => setSearchInput(e.target.value)}
               className="pl-10 border rounded-lg py-2 px-4 w-80 mx-2"
             />
-            <a className="btn bg-violet-700">Submit</a>
+            <a className="btn bg-violet-700">Search</a>
           </div>
         </div>
         <div className="w-screen m-4">
           <div className="grid grid-cols-2 gap-4">
-            {imageIds.map((id) => (
-              <div key={id} className="relative aspect-w-16 aspect-h-9">
+            {landingCardImages.map((landingCardImage, index) => (
+              <div key={index} className="relative aspect-w-16 aspect-h-9">
                 <img
-                  src={`https://picsum.photos/id/${id}/200/300`}
-                  alt={`Image ${id}`}
+                  src={landingCardImage.images.small}
+                  alt={`Landing Card Image ${index}`}
                   className="object-cover"
                 />
               </div>
