db8c4f1baeb6f7cf1450a2cd094c36a3bfb57d93
Xyfalix <129175727+Xyfalix@users.noreply.github.com>
2023-10-27 10:25:39 +0800

    feat(nicholas): add styling and pagination for search results

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,22 +1,22 @@
 export default function Card({cardName, cardImage, setName, setNumber, setTotal, rarity, price}) {
   return (
     <>
-      <div className="w-80 bg-gray-600 flex flex-col items-center">
-        <figure>
-          <img className="m-5"
+      <div className="w-80 bg-gray-600 flex flex-col items-start">
+        <figure className="self-center m-3">
+          <img 
             src={cardImage}
             alt={cardName}
           />
         </figure>
-        <div className="flex flex-col content-end">
+        <div className="flex flex-col pl-10">
           <p className="text-white">{cardName} - {setNumber}/{setTotal}</p>
           <p className="text-white">{rarity}</p>
           <p className="text-white">{setName}</p>
           <p className="text-green-500">
             {price}
           </p>
-          <div className="">
-            <button className="btn btn-sm bg-indigo-700">Buy Now</button>
+          <div>
+            <button className="btn btn-sm bg-indigo-700 pb-2">Buy Now</button>
           </div>
         </div>
       </div>
--- src/pages/SearchResults/SearchResults.jsx
+++ src/pages/SearchResults/SearchResults.jsx
@@ -7,6 +7,8 @@ export default function SearchResults({ firstSearch }) {
   const [user, setUser] = useState(getUser);
   const [searchTerm, setSearchTerm] = useState(firstSearch);
   const [cards, setCards] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const cardsPerPage = 12;
   const apiKey = "e1b93a4d-a6b1-4c49-8dac-c1ed6425dcf6";
 
   function handleSearch(submittedSearch) {
@@ -23,7 +25,7 @@ export default function SearchResults({ firstSearch }) {
           const response = await fetch(apiUrl, {
             method: "GET",
             headers: {
-              "X-Api-Key": apiKey,
+              "X-Api-Key": apiKey
             },
           });
 
@@ -42,12 +44,22 @@ export default function SearchResults({ firstSearch }) {
     fetchCards();
   }, [searchTerm])
 
+  // Calculate the indexes for the current page
+  const indexOfLastCard = currentPage * cardsPerPage;
+  const indexOfFirstCard = indexOfLastCard - cardsPerPage;
+  const currentCards = cards.slice(indexOfFirstCard, indexOfLastCard);
+
+  const paginate = (pageNumber) => setCurrentPage(pageNumber);
+
   return (
     <>
       <NavBar user={user} setUser={setUser} handleSearch={handleSearch} />
       <p>Search Param is {searchTerm}</p>
-      <div className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-        {cards.map((card) => (
+      <div className="w-80 bg-gray-600 m-5 p-3">
+        <h2 className="text-white text-3xl">{cards.length} Products Found</h2>
+      </div>
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 m-5">
+        {currentCards.map((card) => (
           <Card
             key={card.id}
             cardName={card.name}
@@ -61,6 +73,24 @@ export default function SearchResults({ firstSearch }) {
             }
           />
         ))}
+      </div>
+      <div className="pagination-container flex justify-center">
+        <div className="join">
+          {Array.from(
+            { length: Math.ceil(cards.length / cardsPerPage) },
+            (_, index) => (
+              <button
+                key={index}
+                className={`pagination-item btn ${
+                  currentPage === index + 1 ? "btn-active" : ""
+                }`}
+                onClick={() => paginate(index + 1)}
+              >
+                {index + 1}
+              </button>
+            )
+          )}
+        </div>
       </div>
     </>
   );
