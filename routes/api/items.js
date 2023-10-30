d613bc190c51bfde20f27c75a210c38eee2bed00
Xyfalix <129175727+Xyfalix@users.noreply.github.com>
2023-10-30 12:18:16 +0800

    fix(nicholas): fixed item routing paths

--- routes/api/items.js
+++ routes/api/items.js
@@ -3,5 +3,7 @@ const router = express.Router();
 const itemsCtrl = require("../../controllers/api/items");
 const { checkToken } = require("../../config/checkToken");
 
-router.get("/", checkToken, itemsCtrl);
+router.get("/", checkToken, itemsCtrl.getAllItems);
 router.post("/:itemId", checkToken, itemsCtrl.addItem);
+
+module.exports = router;
