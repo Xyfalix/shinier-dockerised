fdc9773ec2e07b5ab442bc710754bdc92b365da3
Xyfalix <129175727+Xyfalix@users.noreply.github.com>
2024-06-29 00:59:33 +0800

    chore: dockerised backend

--- vite.config.js
+++ vite.config.js
@@ -21,13 +21,7 @@ export default defineConfig({
     proxy: {
       "/api": {
         target: "http://localhost:3000",
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ""),
       },
     },
   },
 });
-
-// proxy: {
-//   "/api": "http://localhost:3000",
-// },
